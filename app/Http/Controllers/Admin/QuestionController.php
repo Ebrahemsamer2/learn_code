ae2674db59baad2fa691bc4aa5db16981734400f
Ebrahemsamer2 <ebrahemsamer2@gmail.com>
2019-12-15 09:19:38 +0200

    Finish create function

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -18,26 +18,14 @@ class QuestionController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.questions.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
